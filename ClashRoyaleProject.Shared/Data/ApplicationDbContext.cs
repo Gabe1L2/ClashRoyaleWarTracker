b790885d943b195e6f7d81344098399c0b9e8789
Gabe Lee <gabelee0412@gmail.com>
2025-08-19 17:01:03 -0500

    more stuff

--- ClashRoyaleProject.Shared/Data/ApplicationDbContext.cs
+++ ClashRoyaleProject.Shared/Data/ApplicationDbContext.cs
@@ -10,6 +10,32 @@ namespace ClashRoyaleProject.Shared.Data
             : base(options)
         {
         }
+
         public DbSet<Player> Players { get; set; } = null!;
+        public DbSet<Clan> Clans { get; set; } = null!;
+        public DbSet<ClanHistory> ClanHistories { get; set; } = null!;
+        public DbSet<RawWarData> RawWarData { get; set; } = null!;
+        public DbSet<WarData> WarData { get; set; } = null!;
+        public DbSet<PlayerAverage> PlayerAverages { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure relationships and constraints
+            modelBuilder.Entity<ClanHistory>()
+                .HasOne<Clan>()
+                .WithMany(c => c.Histories)
+                .HasForeignKey(ch => ch.ClanID);
+
+            // Configure unique constraints
+            modelBuilder.Entity<Player>()
+                .HasIndex(p => p.Tag)
+                .IsUnique();
+
+            modelBuilder.Entity<Clan>()
+                .HasIndex(c => c.Tag)
+                .IsUnique();
+        }
     }
 }
--- ClashRoyaleProject.Shared/Models/Player.cs
+++ ClashRoyaleProject.Shared/Models/Player.cs
@@ -2,17 +2,60 @@ namespace ClashRoyaleProject.Shared.Models
 {
     public class Player
     {
-        public string Tag { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
-        public int TrophyCount { get; set; }
+        public int ID { get; set; }
+        public required string Tag { get; set; }
+        public required string Name { get; set; }
         public DateTime LastUpdated { get; set; }
     }
 
     public class Clan
     {
-        public string Tag { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
-        public int MemberCount { get; set; }
+        public int ID { get; set; }
+        public required string Tag { get; set; }
+        public required string Name { get; set; }
+        public int WarTrophies { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+
+    public class ClanHistory
+    {
+        public int ID { get; set; }
+        public int ClanID { get; set; }
+        public int SeasonID { get; set; }
+        public int WeekIndex { get; set; }
+        public int WarTrophies { get; set; }
+        public DateTime RecordedDate { get; set; }
+    }
+
+    public class RawWarData
+    {
+        public int ID { get; set; }
+        public int PlayerID { get; set; }
+        public int ClanHistoryID { get; set; }
+        public int Fame { get; set; }
+        public int RepairPoints { get; set; }
+        public int BoatAttacks { get; set; }
+        public int DecksUsed { get; set; }
+        public DateTime InsertDate { get; set; }
+    }
+
+    public class WarData
+    {
+        public int ID { get; set; }
+        public int PlayerID { get; set; }
+        public int ClanHistoryID { get; set; }
+        public int Fame { get; set; }
+        public int DecksUsed { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+
+    public class PlayerAverage
+    {
+        public int ID { get; set; }
+        public int PlayerID { get; set; }
+        public int ClanID { get; set; }
+        public double FameAttackAverage { get; set; }
+        public bool Is5k { get; set; }
         public DateTime LastUpdated { get; set; }
     }
 }
